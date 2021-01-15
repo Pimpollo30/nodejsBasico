d05141071a598c22b7d225ac472711bbb7647784
Abraham Raymundo <abrahampimpollo@gmail.com>
2021-01-15 08:58:09 -0600

    Preparar para servidor

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.port || 3000;
 
 app.use("/",express.static(path.join(__dirname,'/public')));
 
 
 app.listen(3000, () => {
-	console.log("Servidor escuchando el puerto 3000");
+	console.log("Servidor escuchando el puerto "+port);
 });   
